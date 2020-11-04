b98ef6f5002593f431faff681c36a10bf4f85848
VoyageCN <ieshiaonan@gmail.com>
2020-11-04 16:48:12 +0800

    完善基本CURD处理

--- handler/user/delete.go
+++ handler/user/delete.go
@@ -1,3 +1,18 @@
 package user
 
+import (
+	. "APISERVER/handler"
+	"APISERVER/model"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
 
+func Delete(c *gin.Context) {
+	userId, _ := strconv.Atoi(c.Param("id"))
+	if err := model.DeleteUser(uint64(userId)); err != nil {
+		SendResponse(c, err, nil)
+		return
+	}
+
+	SendResponse(c, nil, nil)
+}
--- handler/user/list.go
+++ handler/user/list.go
@@ -1,1 +1,27 @@
 package user
+
+import (
+	. "APISERVER/handler"
+	"APISERVER/pkg/errno"
+	"APISERVER/service"
+	"github.com/gin-gonic/gin"
+)
+
+func List(c *gin.Context) {
+	var r ListRequest
+	if err := c.Bind(&r); err != nil {
+		SendResponse(c, errno.ErrBind, nil)
+		return
+	}
+
+	infos, count, err := service.ListUser(r.Username, r.Offset, r.Limit)
+	if err != nil {
+		SendResponse(c, err, nil)
+		return
+	}
+
+	SendResponse(c, nil, ListResponse{
+		TotalCount: count,
+		UserList: infos,
+	})
+}

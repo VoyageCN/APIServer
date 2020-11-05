76553525e4deec75e66f0d3ee04d2787117d4274
VoyageCN <ieshiaonan@gmail.com>
2020-11-06 03:37:09 +0800

    加入jwt验证方式

--- handler/user/login.go
+++ handler/user/login.go
@@ -1,7 +1,45 @@
 package user
 
+import (
+	. "APISERVER/handler"
+	"APISERVER/model"
+	"APISERVER/pkg/auth"
+	"APISERVER/pkg/errno"
+	"APISERVER/pkg/token"
+	"github.com/gin-gonic/gin"
+)
 
+// Login generates the authentication token
+// if the password was matched with the specified account.
+func Login(c *gin.Context) {
+	// Binding the data with the use struct.
+	var u model.UserModel
+	if err := c.Bind(&u); err != nil {
+		SendResponse(c, errno.ErrBind, nil)
+		return
+	}
 
+	// Get the user information by the login username.
+	d, err := model.GetUser(u.Username)
+	if err != nil {
+		SendResponse(c, errno.ErrUserNotFound, nil)
+		return
+	}
 
+	// Compare the login password with the user password.
+	if err := auth.Compare(d.Password, u.Password); err != nil {
+		SendResponse(c, errno.ErrPasswordIncorrect, nil)
+		return
+	}
+
+	// Sign the json web token.
+	t, err := token.Sign(c, token.Context{ID: d.Id, Username: d.Username}, "")
+	if err != nil {
+		SendResponse(c, errno.ErrToken, nil)
+		return
+	}
+
+	SendResponse(c, nil, model.Token{Token: t})
+}
 
 
